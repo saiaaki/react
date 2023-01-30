5f20e4889c21cb776b717979d5f5904188572fc8
= <=>
2023-01-30 16:10:49 +0530

    init

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import DataFetching from '../DataFetching'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <DataFetching/>
+  
+         
+    </div>
+  )
+}
+
+export default App
