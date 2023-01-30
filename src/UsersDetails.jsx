2759ebf2d932704deb5e9d3a0e634511ee5d5b99
= <=>
2023-01-30 17:22:37 +0530

    add

--- src/UsersDetails.jsx
+++ src/UsersDetails.jsx
@@ -4,14 +4,16 @@ export const UsersDetails = (props) => {
     const {details} = props
     const {id,subject,body,metrics} = details
   return (
-     <div>
+    <div className='center'> 
+     <div className='msg'>
       
         <p><b>id:</b>{id}</p>
         <p><b>subject:</b>{subject}</p>
-        <p><b>body:</b>{body}</p>
+        <p dangerouslySetInnerHTML={{__html : body}}></p>
         <p><b>views:</b>{metrics.views}</p>
         <hr /> 
       </div>  
+      </div>
      
   )
 }
